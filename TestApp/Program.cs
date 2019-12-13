705c427c66074d9e992521c3b1620253d98ab71f
Wes Hampson <thehambone93@gmail.com>
2019-12-13 00:21:28 -0800

    Add more test data

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -15,16 +15,18 @@ namespace TestApp
             //string savePS22 = homeDir + @"\Desktop\gta\Saves\gta3\ps2_jp\6_LM2";
             //string savePC0 = homeDir + @"\Documents\GTA3 User Files\GTA3sf1.b";
 
-            SaveData gta3Save = SaveData.Load(@".\TestData\PS2JP\1_LM1", SystemType.PS2JP);
-            Console.WriteLine("Save name: " + gta3Save.SimpleVars.LastMissionPassedName);
-            Console.WriteLine("MAIN size: " + gta3Save.Scripts.MainScriptSize);
-            Console.WriteLine("Camera pos: " + gta3Save.SimpleVars.CameraPosition);
-            Console.WriteLine("Global timer: " + gta3Save.SimpleVars.LastClockTick);
-            Console.WriteLine("Weather timer: " + gta3Save.SimpleVars.TimeInMilliseconds);
-            Console.WriteLine("Game time: {0}:{1}", gta3Save.SimpleVars.GameClockHours, gta3Save.SimpleVars.GameClockMinutes);
-            Console.WriteLine("Current weather: " + gta3Save.SimpleVars.NewWeatherType);
+            string inPath = @"..\..\TestData\PC\2_AS3";
+            string outPath = homeDir + @"\Documents\GTA3 User Files\apitest.b";
+            SystemType system = SystemType.PC;
+
+            Console.WriteLine("Loading file...");
+            SaveData gta3Save = SaveData.Load(inPath, system);
+            Console.WriteLine(gta3Save);
             Console.ReadKey();
-            gta3Save.Store(homeDir + @"\Documents\GTA3 User Files\apitest.b");
+
+            gta3Save.Store(outPath, system);
+            Console.WriteLine("File saved.");
+            Console.ReadKey();
         }
 
         private static void BuildingSwaps_ItemPropertyChanged(object sender, ItemPropertyChangedEventArgs e)
