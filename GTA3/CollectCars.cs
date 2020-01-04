36c91e68fd9d1f5779bcbdd1b88a4ef186ac4b07
Wes Hampson <thehambone93@gmail.com>
2020-01-03 18:38:14 -0600

    Use binary syntax for flags enums

--- GTA3/CollectCars.cs
+++ GTA3/CollectCars.cs
@@ -5,60 +5,60 @@ namespace GTASaveData.GTA3
     [Flags]
     public enum CollectCars1
     {
-        Securicar = 1 << 0,
-        Moonbeam = 1 << 1,
-        Coach = 1 << 2,
-        Flatbed = 1 << 3,
-        Linerunner = 1 << 4,
-        Trashmaster = 1 << 5,
-        Patriot = 1 << 6,
-        MrWhoopee = 1 << 7,
-        Blista = 1 << 8,
-        Mule = 1 << 9,
-        Yankee = 1 << 10,
-        Bobcat = 1 << 11,
-        Dodo = 1 << 12,
-        Bus = 1 << 13,
-        Rumpo = 1 << 14,
-        Pony = 1 << 15,
+        Securicar   = 0b_0000_0000_0000_0001,
+        Moonbeam    = 0b_0000_0000_0000_0010,
+        Coach       = 0b_0000_0000_0000_0100,
+        Flatbed     = 0b_0000_0000_0000_1000,
+        Linerunner  = 0b_0000_0000_0001_0000,
+        Trashmaster = 0b_0000_0000_0010_0000,
+        Patriot     = 0b_0000_0000_0100_0000,
+        MrWhoopee   = 0b_0000_0000_1000_0000,
+        Blista      = 0b_0000_0001_0000_0000,
+        Mule        = 0b_0000_0010_0000_0000,
+        Yankee      = 0b_0000_0100_0000_0000,
+        Bobcat      = 0b_0000_1000_0000_0000,
+        Dodo        = 0b_0001_0000_0000_0000,
+        Bus         = 0b_0010_0000_0000_0000,
+        Rumpo       = 0b_0100_0000_0000_0000,
+        Pony        = 0b_1000_0000_0000_0000,
     }
 
     [Flags]
     public enum CollectCars2
     {
-        Sentinel = 1 << 0,
-        Cheetah = 1 << 1,
-        Banshee = 1 << 2,
-        Idaho = 1 << 3,
-        Infernus = 1 << 4,
-        Taxi = 1 << 5,
-        Kuruma = 1 << 6,
-        Stretch = 1 << 7,
-        Perennial = 1 << 8,
-        Stinger = 1 << 9,
-        Manana = 1 << 10,
-        Landstalker = 1 << 11,
-        Stallion = 1 << 12,
-        BFInjection = 1 << 13,
-        Cabbie = 1 << 14,
-        Esperanto = 1 << 15,
+        Sentinel    = 0b_0000_0000_0000_0001,
+        Cheetah     = 0b_0000_0000_0000_0010,
+        Banshee     = 0b_0000_0000_0000_0100,
+        Idaho       = 0b_0000_0000_0000_1000,
+        Infernus    = 0b_0000_0000_0001_0000,
+        Taxi        = 0b_0000_0000_0010_0000,
+        Kuruma      = 0b_0000_0000_0100_0000,
+        Stretch     = 0b_0000_0000_1000_0000,
+        Perennial   = 0b_0000_0001_0000_0000,
+        Stinger     = 0b_0000_0010_0000_0000,
+        Manana      = 0b_0000_0100_0000_0000,
+        Landstalker = 0b_0000_1000_0000_0000,
+        Stallion    = 0b_0001_0000_0000_0000,
+        BFInjection = 0b_0010_0000_0000_0000,
+        Cabbie      = 0b_0100_0000_0000_0000,
+        Esperanto   = 0b_1000_0000_0000_0000,
     }
 
     [Flags]
     public enum CollectCars3
     {
-        Landstalker = 1 << 0
+        Landstalker = 0b_0000_0000_0000_0001
     }
 
     [Flags]
     public enum CollectCarsMilitaryCrane
     {
-        Firetruck = 1 << 0,
-        Ambulance = 1 << 1,
-        Enforcer = 1 << 2,
-        FbiCar = 1 << 3,
-        Rhino = 1 << 4,
-        BarracksOL = 1 << 5,
-        Police = 1 << 6,
+        Firetruck   = 0b_0000_0000_0000_0001,
+        Ambulance   = 0b_0000_0000_0000_0010,
+        Enforcer    = 0b_0000_0000_0000_0100,
+        FbiCar      = 0b_0000_0000_0000_1000,
+        Rhino       = 0b_0000_0000_0001_0000,
+        BarracksOL  = 0b_0000_0000_0010_0000,
+        Police      = 0b_0000_0000_0100_0000,
     }
 }
--- GTA3/StoredCarImmunities.cs
+++ GTA3/StoredCarImmunities.cs
@@ -5,10 +5,10 @@ namespace GTASaveData.GTA3
     [Flags]
     public enum StoredCarImmunities
     {
-        BulletProof = 1 << 0,
-        FireProof = 1 << 1,
-        ExplosionProof = 1 << 2,
-        CollisionProof = 1 << 3,
-        MeleeProof = 1 << 4
+        BulletProof     = 0b_0000_0001,
+        FireProof       = 0b_0000_0010,
+        ExplosionProof  = 0b_0000_0100,
+        CollisionProof  = 0b_0000_1000,
+        MeleeProof      = 0b_0001_0000
     }
 }
