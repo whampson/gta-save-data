5d1f4fc2c6d02e184f9f8df78c034aea1090f32e
Wes Hampson <thehambone93@gmail.com>
2020-01-05 20:03:46 -0800

    Test ByteArrayExtensions

---
+++ GTASaveData.Tests/Extensions/TestByteArrayExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using GTASaveData.Extensions;
+using Xunit;
+
+namespace GTASaveData.Tests.Extensions
+{
+    // TODO: extend to IEnumerable?
+
+    public class TestByteArrayExtensions
+    {
+        [Theory]
+        [InlineData(new byte[] { 1 }, new byte[] { 0 }, -1)]
+        [InlineData(new byte[] { 0 }, new byte[] { 0 }, 0)]
+        [InlineData(new byte[] { 1, 0, 1 }, new byte[] { 1 }, 0)]
+        [InlineData(new byte[] { 1, 0, 1 }, new byte[] { 0 }, 1)]
+        [InlineData(new byte[] { 1, 3, 3, 1 }, new byte[] { 1, 3, }, 0)]
+        [InlineData(new byte[] { 1, 3, 3, 1 }, new byte[] { 3, 3, }, 1)]
+        [InlineData(new byte[] { 1, 3, 3, 1 }, new byte[] { 3, 1, }, 2)]
+        public void FindFirst(byte[] buf, byte[] seq, int expectedIndex)
+        {
+            int index = buf.FindFirst(seq);
+
+            Assert.Equal(expectedIndex, index);
+        }
+    }
+}
