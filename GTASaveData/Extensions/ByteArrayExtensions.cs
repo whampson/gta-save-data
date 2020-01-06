ccf9932fc81cb4a3c694d8d734eb5016c48d12ba
Wes Hampson <thehambone93@gmail.com>
2020-01-05 20:03:21 -0800

    Add ByteArrayExtensions and StringExtensions

---
+++ GTASaveData/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace GTASaveData.Extensions
+{
+    public static class ByteArrayExtensions
+    {
+        public static int FindFirst(this byte[] b, byte[] seq, int start = 0)
+        {
+            int searchLen = b.Length - seq.Length + 1;
+
+            if (b == null || seq == null ||
+                b.Length == 0 || seq.Length == 0 ||
+                b.Length < seq.Length || start < 0 ||
+                start >= searchLen)
+            {
+                return -1;
+            }
+
+            int index = -1;
+            for (int i = start; i < searchLen; i++)
+            {
+                if (b[i] == seq[0])
+                {
+                    // First byte match, kick off lookahead routine
+                    bool match = true;
+                    for (int k = 1; k < b.Length - 1 && k < seq.Length; k++)
+                    {
+                        if (b[i + k] != seq[k])
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+
+                    if (match)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ GTASaveData/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace GTASaveData.Extensions
+{
+    public static class StringExtensions
+    {
+        public static byte[] GetAsciiBytes(this string s)
+        {
+            return Encoding.ASCII.GetBytes(s);
+        }
+    }
+}
