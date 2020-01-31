525ba73ff2652bd0ef6d7cd367fdc780d894b71f
Wes Hampson <thehambone93@gmail.com>
2020-01-31 15:18:35 -0800

    Add TypeExtensions

---
+++ GTASaveData/Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace GTASaveData.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static bool IsObservable(this Type t)
+        {
+            if (t == null)
+            {
+                return false;
+            }
+
+            return t.GetInterface(nameof(INotifyPropertyChanged)) != null;
+        }
+    }
+}
