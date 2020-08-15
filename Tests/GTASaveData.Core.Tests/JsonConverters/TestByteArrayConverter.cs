cdb13fea1f3dd6c1313c848b1acdb78066b71a85
Wes Hampson <thehambone93@gmail.com>
2020-08-15 13:53:15 -0700

    Add converter tests

---
+++ Tests/GTASaveData.Core.Tests/JsonConverters/TestByteArrayConverter.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using GTASaveData.JsonConverters;
+using Newtonsoft.Json;
+using TestFramework;
+using Xunit;
+
+namespace GTASaveData.Core.Tests.JsonConverters
+{
+    public class TestByteArrayConverter : TestBase
+    {
+        [Fact]
+        public void ConvertBelowThreshold()
+        {
+            Faker f = new Faker();
+            TestClass x = new TestClass
+            {
+                Data = f.Random.Bytes(f.Random.Int(0, ByteArrayConverter.DefaultThreshold))
+            };
+
+            string json = JsonConvert.SerializeObject(x);
+            TestClass y = JsonConvert.DeserializeObject<TestClass>(json);
+
+            Assert.Equal(x.Data, y.Data);
+        }
+
+        [Fact]
+        public void ConvertAboveThreshold()
+        {
+            Faker f = new Faker();
+            TestClass x = new TestClass
+            {
+                Data = f.Random.Bytes(f.Random.Int(ByteArrayConverter.DefaultThreshold, 1000))
+            };
+
+            string json = JsonConvert.SerializeObject(x);
+            TestClass y = JsonConvert.DeserializeObject<TestClass>(json);
+
+            Assert.Equal(x.Data, y.Data);
+        }
+    }
+
+    public class TestClass
+    {
+        [JsonConverter(typeof(ByteArrayConverter))]
+        public Array<byte> Data;
+    }
+}
---
+++ Tests/GTASaveData.Core.Tests/JsonConverters/TestIntArrayConverter.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Bogus;
+using GTASaveData.JsonConverters;
+using Newtonsoft.Json;
+using TestFramework;
+using Xunit;
+
+namespace GTASaveData.Core.Tests.JsonConverters
+{
+    public class TestIntArrayConverter : TestBase
+    {
+        [Fact]
+        public void ConvertBelowThreshold()
+        {
+            Faker f = new Faker();
+            TestClass x = new TestClass
+            {
+                Data = Generator.Array(f.Random.Int(0, IntArrayConverter.Threshold), g => f.Random.Int())
+            };
+
+            string json = JsonConvert.SerializeObject(x);
+            TestClass y = JsonConvert.DeserializeObject<TestClass>(json);
+
+            Assert.Equal(x.Data, y.Data);
+        }
+
+        [Fact]
+        public void ConvertAboveThreshold()
+        {
+            Faker f = new Faker();
+            TestClass x = new TestClass
+            {
+                Data = Generator.Array(f.Random.Int(IntArrayConverter.Threshold, 1000), g => f.Random.Int())
+            };
+
+            string json = JsonConvert.SerializeObject(x);
+            TestClass y = JsonConvert.DeserializeObject<TestClass>(json);
+
+            Assert.Equal(x.Data, y.Data);
+        }
+
+        public class TestClass
+        {
+            [JsonConverter(typeof(IntArrayConverter))]
+            public Array<int> Data;
+        }
+    }
+}
