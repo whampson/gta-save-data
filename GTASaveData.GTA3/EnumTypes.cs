da379d06558bc4391fe6b3dd50ff7b2001cea3b7
Wes Hampson <thehambone93@gmail.com>
2021-12-21 16:56:22 -0600

    Remove descriptions, add CameraMode

--- GTASaveData.GTA3/EnumTypes.cs
+++ GTASaveData.GTA3/EnumTypes.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace GTASaveData.GTA3
 {
     public enum BombType
     {
-        [Description("(none)")]
         None,
-
-        [Description("Timer")]
         Timer,
-
-        [Description("Ignition")]
         OnIgnition,
-
-        [Description("Remote")]
         Remote,
-
-        [Description("Timer (armed)")]
         TimerArmed,
-
-        [Description("Ignition (armed)")]
         OnIgnitionArmed
+    }
+
+    public enum CameraMode
+    {
+        None,
+        Bumper = 0,
+        Near,
+        Middle,
+        Far,
+        TopDown,
+        Cinematic
     }
 
     public enum GarageState
@@ -36,7 +35,6 @@ namespace GTASaveData.GTA3
 
     public enum GarageType
     {
-        [Description("(none)")]
         None,
         Mission,
         BombShop1,
@@ -73,64 +71,32 @@ namespace GTASaveData.GTA3
 
     public enum Level
     {
-        [Description("(none)")]
         None,
-
-        [Description("Portland")]
         Industrial,
-
-        [Description("Staunton Island")]
         Commercial,
-
-        [Description("Shoreside Vale")]
         Suburban
     }
 
     public enum QuickSaveState
     {
-        [Description("(none)")]
         None,
-
         Normal,
         OnMission
     }
 
     public enum RadioStation
     {
-        [Description("Head Radio")]
         HeadRadio,
-
-        [Description("Double Cleff FM")]
         DoubleClef,
-
-        [Description("Jah Radio")]
         JahRadio,
-
-        [Description("Rise FM")]
         RiseFM,
-
-        [Description("Lips 106")]
         Lips106,
-
-        [Description("Game FM")]
         GameFM,
-
-        [Description("MSX FM")]
         MsxFM,
-
-        [Description("Flashback 95.6")]
         Flashback,
-
-        [Description("Chatterbox 109")]
         Chatterbox,
-
-        [Description("MP3 Player")]
         UserTrack,
-
-        [Description("Police Radio")]
         PoliceRadio,
-
-        [Description("(none)")]
         None
     }
 
@@ -146,9 +112,7 @@ namespace GTASaveData.GTA3
 
     public enum WeatherType
     {
-        [Description("(none)")]
         None = -1,
-
         Sunny,
         Cloudy,
         Rainy,
